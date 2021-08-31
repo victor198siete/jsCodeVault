8f7ef7f4da555ee47b13384c107fc40b25b7b127
victor198siete <victor198siete@gmail.com>
2021-08-31 17:38:07 -0500

    JS course commit,Data Structures Operators (...Rest Pattern and Parameters)

--- 09-Data-Structures-Operators/script.js
+++ 09-Data-Structures-Operators/script.js
@@ -21,8 +21,6 @@ const restaurant = {
     console.log(`Here is your delicious pasta with ${ing1}, ${ing2} and ${ing3}`);
   },
 
-
-
   openingHours: {
     thu: {
       open: 12,
@@ -37,6 +35,11 @@ const restaurant = {
       close: 24,
     },
   },
+
+  orderPizza: function(mainIngredient, ...otherIngredients)  {
+      console.log(mainIngredient);
+      console.log(otherIngredients);
+  }
 };
 
 //103. Destructuring Arrays
@@ -139,7 +142,7 @@ console.log(mainMenuCopy);
 const fullMenu = [...restaurant.starterMenu, ...restaurant.mainMenu];
 console.log(fullMenu);
 
-//Iterables: arrays, strings, maps, sets
+//Iterables: arrays, strings, maps, sets. Not Objects!!!
 const str = 'Victor';
 const letters = [...str, '', 'M.'];
 console.log(letters);
@@ -161,6 +164,46 @@ const restaurantCopy = {...newRestaurant};
 restaurantCopy.name = 'Rome Empire';
 console.log(restaurantCopy.name);
 console.log(newRestaurant.name);
+
+//106. Rest Pattern and Parameters
+
+// 1 Part - Destructuring
+
+//Spread, because on right side of =
+const arrEx = [1, 2, ...[3, 4, 5]];
+
+//Rest, because on left side of =
+const [l, m,...others] = [1, 2, 3, 4, 5];
+console.log(l, m, others);
+
+const [pizza, ,risotto, ...otherFood] = [...restaurant.mainMenu, ...restaurant.starterMenu];
+console.log(pizza, risotto, otherFood);
+
+//Objects
+const { sat, ...weekdays } = restaurant.openingHours;
+console.log(sat, weekdays);
+
+// 2 Part - Functions
+const add = function(...numbers) {
+    let sum = 0;
+    for (let i = 0; i < numbers.length; i++) {
+        sum += numbers[i];
+    }
+    console.log(sum);
+};
+
+add(2, 3);
+add(5, 3, 7, 2);
+add(8, 2, 5, 3, 2, 1, 4);
+
+const ex = [23, 5, 7];
+add(...ex);
+
+restaurant.orderPizza('mushrooms', 'onions', 'olives', 'spinach');
+
+restaurant.orderPizza('mushrooms');
+
+
 
 // Data needed for a later exercise
 //const flights =
